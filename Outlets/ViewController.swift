cd2e63efc0e0e5436ede7cc11685dde99eccbdc1
José Trejo <joseatrejos>
2022-08-16 09:39:15 -0700

    Primer commit

--- Outlets/ViewController.swift
+++ Outlets/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Un outlet es una conexion entre un Control de UI del Storyboard y una variable de Codigo Swift
+    @IBOutlet weak var lblMiEtiqueta: UILabel!
+    
+    
+    //Esta funcion se ejecuta ya que cargo todo en memoria (antes de aparecer en pantalla)
+    //Lo que pongamos aqui sobreescribe lo del storyboard
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        lblMiEtiqueta.text = "Este es texto desde codigo"
+        lblMiEtiqueta.textColor = UIColor.cyan
     }
 
 
